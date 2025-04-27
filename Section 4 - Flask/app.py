a7730fe8ed216075acd3728535695f313ef3a282
sahil095 <sehgal.sahil0786@gmail.com>
2025-04-27 14:13:10 -0400

    added flask

---
+++ Section 4 - Flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+'''
+    It creates an instance of the Flask class,
+    which will be your WSGI (Web Server Gateway Interface) application.
+'''
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to Flask tutorial"
+
+
+if __name__ == "__main__":
+    app.run()
